78f86201bf1f10f2be52621a336840bceaaee311
Abhishek Neti <abhineti239@gmail.com>
2023-08-21 19:17:29 +0530

    added timezone

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
-
+import { useState, useEffect } from 'react';
 function App() {
-	return <div className='App'>This is my first azure react app</div>;
+	const [time, setTime] = useState(new Date());
+
+	useEffect(() => {
+		setInterval(() => {
+			setTime(new Date());
+		}, 1000);
+	}, []);
+
+	return (
+		<>
+			<div className='App'>
+				This is my first azure react app. current time is {time.toTimeString()}
+			</div>
+		</>
+	);
 }
 
 export default App;
